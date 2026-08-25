223f6133f3841b56fc551143db070c5a5c5eace0
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-25 17:17:02 +0530

    Added Create Student Responce Dto

--- student-crud/src/main/java/com/example/student_crud/controller/StudentController.java
+++ student-crud/src/main/java/com/example/student_crud/controller/StudentController.java
@@ -1,10 +1,12 @@
 package com.example.student_crud.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
+
 
 }
