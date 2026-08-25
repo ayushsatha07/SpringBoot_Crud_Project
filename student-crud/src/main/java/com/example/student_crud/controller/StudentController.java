56ed77d6570bf8f42b8e3084d8e35833e1fee496
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-25 17:30:25 +0530

    Added StudentService Dependency in Conroller

--- student-crud/src/main/java/com/example/student_crud/controller/StudentController.java
+++ student-crud/src/main/java/com/example/student_crud/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.student_crud.controller;
 
+import com.example.student_crud.dto.CreateStudentReqDto;
+import com.example.student_crud.dto.CreateStudentRespDto;
+import com.example.student_crud.service.StudentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
 
 
 }
