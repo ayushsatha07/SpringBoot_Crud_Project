e79f7ef23c2f4128835c4d22e5250fa031363d12
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-25 17:25:42 +0530

    Added request fields on createStudent--->Dto

--- student-crud/src/main/java/com/example/student_crud/dto/CreateStudentReqDto.java
+++ student-crud/src/main/java/com/example/student_crud/dto/CreateStudentReqDto.java
@@ -1,4 +1,87 @@
 package com.example.student_crud.dto;
 
+import java.time.LocalDate;
+
 public class CreateStudentReqDto {
+    private String name;
+    private String email;
+    private String phone;
+    private String gender;
+    private LocalDate dob;
+    private String branch;
+    private int year;
+    private String rollNo;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
