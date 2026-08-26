7c4a50124e02156aad68874c667ecf736b2d83a1
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-26 16:49:24 +0530

    Added Depenedency of StudentRepository

--- student-crud/src/main/java/com/example/student_crud/service/StudentService.java
+++ student-crud/src/main/java/com/example/student_crud/service/StudentService.java
@@ -1,8 +1,17 @@
 package com.example.student_crud.service;
 
+import com.example.student_crud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private StudentRepository repository;
+
+    @Autowired
+    public StudentService(StudentRepository repository){
+        this.repository = repository;
+    }
 
 }
