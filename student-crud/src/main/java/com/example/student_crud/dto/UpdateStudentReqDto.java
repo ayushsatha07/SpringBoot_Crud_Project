3f273046b321df84f4343d454ad71a247010fc73
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-27 11:52:33 +0530

    Added Update Student Req Dto

---
+++ student-crud/src/main/java/com/example/student_crud/dto/UpdateStudentReqDto.java
@@ -0,0 +1,7 @@
+package com.example.student_crud.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateStudentReqDto {
+}
---
+++ student-crud/src/main/java/com/example/student_crud/dto/UpdateStudentRespDto.java
@@ -0,0 +1,4 @@
+package com.example.student_crud.dto;
+
+public class UpdateStudentRespDto {
+}
